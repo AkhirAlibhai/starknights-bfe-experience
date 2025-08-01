fc1f99979592094b2531e5db4a22d2ef4aadc768
Computed Functions <215638926+computed-fn@users.noreply.github.com>
2025-08-01 20:58:46 +0700

    Preview

--- src/pages/calls/index.vue
+++ src/pages/calls/index.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
-import { useDate, useTheme } from "vuetify";
+import { useDate } from "vuetify";
+import Sample from "@/assets/sample.wav";
 
 const dateUtils = useDate();
 const randDate = ref(dateUtils.format(new Date(), "fullDateTime24h"));
+const opened = ref([]);
+
+const container = useTemplateRef("audioContainer");
+const audio = useMediaControls(container);
+const displayTime = computed(() => {
+  const sec = Math.round(audio.currentTime.value % 60);
+  const min = Math.floor(audio.currentTime.value / 60);
+  return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`
+});
+const displayDuration = computed(() => {
+  const sec = Math.round(audio.duration.value % 60);
+  const min = Math.floor(audio.duration.value / 60);
+  return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`
+});
 </script>
 
 <template>
@@ -10,9 +25,8 @@ const randDate = ref(dateUtils.format(new Date(), "fullDateTime24h"));
     <VContainer fluid>
       <VRow>
         <VCol>
-          <VTextField single-line clearable persistent-clear density="compact" rounded bg-color="#ffffff"
-            variant="solo" hide-details placeholder="Number or contact name" prepend-inner-icon="$search"
-            label="Search"></VTextField>
+          <VTextField single-line clearable persistent-clear density="compact" rounded bg-color="#ffffff" variant="solo"
+            hide-details placeholder="Number or contact name" prepend-inner-icon="$search" label="Search"></VTextField>
         </VCol>
       </VRow>
       <VRow>
@@ -22,85 +36,165 @@ const randDate = ref(dateUtils.format(new Date(), "fullDateTime24h"));
       </VRow>
       <VRow>
         <VCol>
-          <VList density="comfortable" rounded="xl">
-            <VListItem to="/calls/someid" prepend-avatar="https://i.pravatar.cc/300?u=001" title="long ass starknights name. idk why" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail" color="success"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
+          <VList density="comfortable" rounded="xl" open-strategy="single" v-model:opened="opened">
+            <VListGroup value="something">
+              <template #activator="{ props }">
+                <VListItem prepend-avatar="https://i.pravatar.cc/300?u=001" title="long ass starknights name. idk why"
+                  v-bind="props">
+                  <template #subtitle>
+                    <VLabel>
+                      <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
+                      <VIcon icon="$voicemail" color="success"></VIcon>&nbsp;
+                      {{ randDate }}
+                    </VLabel>
+                  </template>
+                </VListItem>
               </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=002" title="Starknight #002" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail" color="success"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
+              <VCard>
+                <VCardText>
+                  <audio ref="audioContainer" :src="Sample"></audio>
+                  <VRow>
+                    <VCol cols="12">
+                      <VSlider min="0" :max="audio.duration.value" v-model="audio.currentTime.value" hide-details
+                        class="-mt-4" density="compact" track-size="2px" thumb-size="12px"></VSlider>
+                    </VCol>
+                  </VRow>
+                  <VRow dense>
+                    <VCol cols="3">{{ displayTime }}</VCol>
+                    <VCol cols="6"></VCol>
+                    <VCol cols="3" class="text-right">{{ displayDuration }}</VCol>
+                  </VRow>
+                  <VRow dense>
+                    <VCol cols="2">
+                      <VBtn :icon="!audio.playing.value ? '$play' : '$pause'" color="white" density="compact"
+                        @click="audio.playing.value = !audio.playing.value"></VBtn>
+                    </VCol>
+                    <VCol cols="2">
+                      <VBtn :icon="!audio.muted.value ? '$speaker' : '$mute'" color="white" density="compact"
+                        @click="audio.muted.value = !audio.muted.value"></VBtn>
+                    </VCol>
+                    <VCol cols="4"></VCol>
+                    <VCol cols="2" class="text-right">
+                      <VBtn icon="$stop" color="white" density="compact"
+                        @click="() => { audio.playing.value = false; audio.currentTime.value = 0 }"></VBtn>
+                    </VCol>
+                    <VCol cols="2" class="text-right">
+                      <VBtn icon="$read" color="white" density="compact"></VBtn>
+                    </VCol>
+                  </VRow>
+                  <VRow>
+                    <VList density="comfortable">
+                      <VListItem prepend-icon="$call" title="Voice Call" to="/calls/001"></VListItem>
+                      <VListItem prepend-icon="$vidcall" title="Video Call" to="/calls/001?video=true"></VListItem>
+                      <VListItem prepend-icon="$text" title="Send a Message" disabled></VListItem>
+                    </VList>
+                  </VRow>
+                </VCardText>
+              </VCard>
+              <VDivider></VDivider>
+            </VListGroup>
+            <VListGroup value="starknight002">
+              <template #activator="{ props }">
+                <VListItem prepend-avatar="https://i.pravatar.cc/300?u=002" title="Starknight#002" v-bind="props">
+                  <template #subtitle>
+                    <VLabel>
+                      <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
+                      <VIcon icon="$voicemail"></VIcon>&nbsp;
+                      {{ randDate }}
+                    </VLabel>
+                  </template>
+                </VListItem>
               </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=003" title="Starknight #003" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail" color="success"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
+              <VCard>
+                <VCardText>
+                  <VRow>
+                    <VCol cols="12">
+                      <VSlider hide-details class="-mt-4" density="compact" track-size="2px" thumb-size="12px">
+                      </VSlider>
+                    </VCol>
+                  </VRow>
+                  <VRow dense>
+                    <VCol cols="3">00.00</VCol>
+                    <VCol cols="6"></VCol>
+                    <VCol cols="3" class="text-right">99.99</VCol>
+                  </VRow>
+                  <VRow dense>
+                    <VCol cols="2">
+                      <VBtn icon="$play" color="white" density="compact"></VBtn>
+                    </VCol>
+                    <VCol cols="2">
+                      <VBtn icon="$speaker" color="white" density="compact"></VBtn>
+                    </VCol>
+                    <VCol cols="4"></VCol>
+                    <VCol cols="2" class="text-right">
+                      <VBtn icon="$stop" color="white" density="compact"></VBtn>
+                    </VCol>
+                    <VCol cols="2" class="text-right">
+                      <VBtn icon="$read" color="white" density="compact"></VBtn>
+                    </VCol>
+                  </VRow>
+                  <VRow>
+                    <VList density="comfortable">
+                      <VListItem prepend-icon="$call" title="Voice Call"></VListItem>
+                      <VListItem prepend-icon="$vidcall" title="Video Call"></VListItem>
+                      <VListItem prepend-icon="$text" title="Send a Message"></VListItem>
+                    </VList>
+                  </VRow>
+                </VCardText>
+              </VCard>
+              <VDivider></VDivider>
+            </VListGroup>
+            <VListGroup value="starknight003">
+              <template #activator="{ props }">
+                <VListItem prepend-avatar="https://i.pravatar.cc/300?u=003" title="Starknight#003" v-bind="props">
+                  <template #subtitle>
+                    <VLabel>
+                      <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
+                      <VIcon icon="$voicemail"></VIcon>&nbsp;
+                      {{ randDate }}
+                    </VLabel>
+                  </template>
+                </VListItem>
               </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=004" title="Starknight #004" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=005" title="Starknight #005" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=006" title="Starknight #006" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=007" title="Starknight #007" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=008" title="Starknight #008" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=009" title="Starknight #009" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=010" title="Starknight #010" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=011" title="Starknight #011" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=012" title="Starknight #012" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
-            <VListItem prepend-avatar="https://i.pravatar.cc/300?u=013" title="Starknight #008" :subtitle="randDate">
-              <template #append>
-                <VIcon icon="$voicemail"></VIcon>
-                <VIcon icon="$callMissed" color="error" class="ms-2"></VIcon>
-              </template>
-            </VListItem>
+              <VCard>
+                <VCardText>
+                  <VRow>
+                    <VCol cols="12">
+                      <VSlider hide-details class="-mt-4" density="compact" track-size="2px" thumb-size="12px">
+                      </VSlider>
+                    </VCol>
+                  </VRow>
+                  <VRow dense>
+                    <VCol cols="3">00.00</VCol>
+                    <VCol cols="6"></VCol>
+                    <VCol cols="3" class="text-right">99.99</VCol>
+                  </VRow>
+                  <VRow dense>
+                    <VCol cols="2">
+                      <VBtn icon="$play" color="white" density="compact"></VBtn>
+                    </VCol>
+                    <VCol cols="2">
+                      <VBtn icon="$speaker" color="white" density="compact"></VBtn>
+                    </VCol>
+                    <VCol cols="4"></VCol>
+                    <VCol cols="2" class="text-right">
+                      <VBtn icon="$stop" color="white" density="compact"></VBtn>
+                    </VCol>
+                    <VCol cols="2" class="text-right">
+                      <VBtn icon="$read" color="white" density="compact"></VBtn>
+                    </VCol>
+                  </VRow>
+                  <VRow>
+                    <VList density="comfortable">
+                      <VListItem prepend-icon="$call" title="Voice Call"></VListItem>
+                      <VListItem prepend-icon="$vidcall" title="Video Call"></VListItem>
+                      <VListItem prepend-icon="$text" title="Send a Message"></VListItem>
+                    </VList>
+                  </VRow>
+                </VCardText>
+              </VCard>
+              <VDivider></VDivider>
+            </VListGroup>
           </VList>
         </VCol>
       </VRow>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -14,9 +14,18 @@ import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 
 // Icons
 
+import IconText from "~icons/material-symbols/android-messages";
+import IconPhone from "~icons/material-symbols/call";
 import IconCallMissed from "~icons/material-symbols/call-missed";
+import IconRead from "~icons/material-symbols/check";
+import IconPause from "~icons/material-symbols/pause";
+import IconPlay from "~icons/material-symbols/play-arrow";
 import IconSearch from "~icons/material-symbols/search";
+import IconStop from "~icons/material-symbols/stop";
+import IconVidcall from "~icons/material-symbols/video-call";
 import IconVoicemail from "~icons/material-symbols/voicemail";
+import IconMute from "~icons/material-symbols/volume-off";
+import IconSpeaker from "~icons/material-symbols/volume-up";
 
 const Register: Plugin = (app) => {
 	return app.use(
@@ -32,6 +41,15 @@ const Register: Plugin = (app) => {
 					search: IconSearch,
 					callMissed: IconCallMissed,
 					voicemail: IconVoicemail,
+					play: IconPlay,
+					speaker: IconSpeaker,
+					stop: IconStop,
+					read: IconRead,
+					mute: IconMute,
+					pause: IconPause,
+					call: IconPhone,
+					vidcall: IconVidcall,
+					text: IconText,
 				},
 				sets: {
 					mdi,
