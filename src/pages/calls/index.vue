65a2f54a910944d30060065ea4bed9c22628b71b
Akhir Alibhai <akhir.alibhai@gmail.com>
2025-10-07 00:31:30 -0400

    Add scuffed vibe coded stuff

--- src/pages/calls/index.vue
+++ src/pages/calls/index.vue
@@ -5,18 +5,14 @@ import callData from "@/data/calls.json";
 const dateUtils = useDate();
 const opened = ref<number[]>([]);
 
-const container = useTemplateRef("audioContainer");
-const audio = useMediaControls(container);
-const displayTime = computed(() => {
-  const sec = Math.round(audio.currentTime.value % 60);
-  const min = Math.floor(audio.currentTime.value / 60);
-  return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`
-});
-const displayDuration = computed(() => {
-  const sec = Math.round(audio.duration.value % 60);
-  const min = Math.floor(audio.duration.value / 60);
-  return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`
-});
+const audioRefs = ref<Record<number, HTMLAudioElement>>({});
+const audioStates = ref<Record<number, { currentTime: number; duration: number; playing: boolean }>>({});
+
+const displayTime = (time: number) => {
+  const sec = Math.round(time % 60);
+  const min = Math.floor(time / 60);
+  return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
+};
 
 interface CallLog {
   id: number
@@ -47,6 +43,51 @@ function readCallsConfig(): CallLog[] {
 
 const callLogs = ref<CallLog[]>(readCallsConfig());
 
+function initializeAudio(log: CallLog) {
+  if (!audioRefs.value[log.id]) {
+    const audioElement = new Audio(log.audio);
+    audioRefs.value[log.id] = audioElement;
+
+    audioStates.value[log.id] = {
+      currentTime: 0,
+      duration: 0,
+      playing: false,
+    };
+
+    audioElement.addEventListener("loadedmetadata", () => {
+      audioStates.value[log.id].duration = audioElement.duration;
+    });
+
+    audioElement.addEventListener("timeupdate", () => {
+      audioStates.value[log.id].currentTime = audioElement.currentTime;
+    });
+
+    audioElement.addEventListener("play", () => {
+      audioStates.value[log.id].playing = true;
+    });
+    audioElement.addEventListener("pause", () => {
+      audioStates.value[log.id].playing = false;
+    });
+  }
+}
+
+function togglePlay(logId: number) {
+  const audio = audioRefs.value[logId];
+  if (audio) {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
+}
+
+function updateCurrentTime(logId: number, value: number) {
+  const audio = audioRefs.value[logId];
+  if (audio) {
+    audio.currentTime = value; // Update the currentTime of the audio element
+  }
+}
 </script>
 
 <template>
@@ -66,8 +107,7 @@ const callLogs = ref<CallLog[]>(readCallsConfig());
       <VRow>
         <VCol>
           <VList density="comfortable" rounded="xl" open-strategy="single" v-model:opened="opened">
-            <VListGroup v-for="log in callLogs" :key="log.id" :value="log.id"
-              :prepend-icon="opened.includes(log.id) ? '$expandLess' : '$expandMore'">
+            <VListGroup v-for="log in callLogs" :key="log.id" :value="log.id" @click="initializeAudio(log)">
               <template #activator="{ props }">
                 <VListItem :prepend-avatar="log.avatar" :title="log.title" v-bind="props">
                   <template #subtitle>
@@ -81,31 +121,35 @@ const callLogs = ref<CallLog[]>(readCallsConfig());
               </template>
               <VCard>
                 <VCardText>
-                  <audio ref="audioContainer" :src="log.audio" />
                   <VRow>
                     <VCol cols="12">
-                      <VSlider min="0" :max="audio.duration.value" v-model="audio.currentTime.value" hide-details
-                        class="-mt-4" density="compact" track-size="2px" thumb-size="12px"></VSlider>
+                      <VSlider v-if="audioStates[log.id]" :min="0" :max="audioStates[log.id].duration"
+                        :model-value="audioStates[log.id].currentTime"
+                        @update:model-value="updateCurrentTime(log.id, $event)" hide-details class="-mt-4"
+                        density="compact" track-size="2px" thumb-size="12px"></VSlider>
                     </VCol>
                   </VRow>
                   <VRow dense>
-                    <VCol cols="3">{{ displayTime }}</VCol>
+                    <VCol cols="3">{{ displayTime(audioStates[log.id]?.currentTime || 0) }}</VCol>
                     <VCol cols="6"></VCol>
-                    <VCol cols="3" class="text-right">{{ displayDuration }}</VCol>
+                    <VCol cols="3" class="text-right">{{ displayTime(audioStates[log.id]?.duration || 0) }}</VCol>
                   </VRow>
                   <VRow dense>
                     <VCol cols="2">
-                      <VBtn :icon="!audio.playing.value ? '$play' : '$pause'" color="white" density="compact"
-                        @click="audio.playing.value = !audio.playing.value"></VBtn>
+                      <VBtn v-if="audioStates[log.id]" :icon="!audioStates[log.id].playing ? '$play' : '$pause'"
+                        color="white" density="compact" @click="togglePlay(log.id)"></VBtn>
                     </VCol>
                     <VCol cols="2">
-                      <VBtn :icon="!audio.muted.value ? '$speaker' : '$mute'" color="white" density="compact"
-                        @click="audio.muted.value = !audio.muted.value"></VBtn>
+                      <VBtn v-if="audioRefs[log.id]" :icon="audioRefs[log.id]?.muted ? '$mute' : '$speaker'"
+                        color="white" density="compact" @click="audioRefs[log.id].muted = !audioRefs[log.id].muted">
+                      </VBtn>
                     </VCol>
                     <VCol cols="4"></VCol>
                     <VCol cols="2" class="text-right">
-                      <VBtn icon="$stop" color="white" density="compact"
-                        @click="() => { audio.playing.value = false; audio.currentTime.value = 0 }"></VBtn>
+                      <VBtn v-if="audioRefs[log.id]" icon="$stop" color="white" density="compact" @click="() => {
+                        audioRefs[log.id].pause();
+                        audioRefs[log.id].currentTime = 0;
+                      }"></VBtn>
                     </VCol>
                     <VCol cols="2" class="text-right">
                       <VBtn icon="$read" color="white" density="compact"></VBtn>
