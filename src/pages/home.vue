ca24625eb5c6e768a9ec050a4ca510e395a84b92
Akhir Alibhai <akhir.alibhai@gmail.com>
2025-12-07 22:49:49 -0500

    Fix some formatting

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -11,7 +11,7 @@ import AppIcon from "@/components/AppIcon.vue";
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-6 p-6 text-white h-full">
+  <div class="grid grid-cols-3 auto-rows-max items-start justify-items-center gap-6 p-6 text-white h-full">
     <AppIcon v-for="a in apps" :key="a.id" :app="a" />
   </div>
 </template>
